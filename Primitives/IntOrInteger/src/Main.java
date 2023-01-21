f6911f33835ed1622d90a2119db86c0d0d79994e
Alexey Pustovalov <54944468+frenzydrive@users.noreply.github.com>
2023-01-21 10:02:29 +0300

    Add files via upload

---
+++ Primitives/IntOrInteger/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задание №1:");
+        Container container = new Container();
+        container.addCount(5672);
+        System.out.println(container.getCount());
+        System.out.println();
+        System.out.println("Задание №2:");
+
+        // TODO: ниже напишите код для выполнения задания:
+        //  С помощью цикла и преобразования чисел в символы найдите все коды
+        //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
+
+        for (int i = 0; i < 65536; i++) {
+            char c = (char) i;
+            if (c == 'Ё' || c == 'ё') {
+                System.out.println(i + " - " + c);
+            } else if (c >= 'А' && c <= 'я') {
+                System.out.println(i + " - " + c);
+            }
+        }
+
+
+    }
+}
